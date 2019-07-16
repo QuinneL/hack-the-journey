139618d2b556acfd74da4ef7697b3ae8160ce4d3
Quinnel <helenliu555@gmail.com>
2019-07-16 18:37:51 -0400

    running main now works

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from amadeus import Client, ResponseError
+from parsing_amadeus import parseAmadeus
+from datetime import datetime, time, date 
 
 amadeus = Client(
     client_id='CEQgUXrPgPn5oG6KFk2BF2hvqlaZh8I3',
@@ -8,3 +10,13 @@ amadeus = Client(
 #in real life the app would fetch information
 profile_file = input('Name of profile json file: ')
 trip_file = input('Name of trip json file: ')
+
+trip_lat = 41.397158
+trip_long = 2.160873
+city_radius = 2
+try:
+    response = amadeus.reference_data.locations.points_of_interest.get(latitude=trip_lat, longitude=trip_long, radius=city_radius)
+except ResponseError as error:
+    print(error)
+
+parseAmadeus(response.data)
