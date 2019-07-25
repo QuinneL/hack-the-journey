ad60046e394c5a4b62c2803a227ea5ffb7606e62
Quinnel <helenliu555@gmail.com>
2019-07-24 23:04:01 -0400

    set up times

--- parsing_json.py
+++ parsing_json.py
@@ -1,6 +1,7 @@
 import json
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="TOTO")
+from datetime import datetime, date
 
 class UserProfile:
     name = ""
@@ -11,8 +12,8 @@ class UserProfile:
     date = ""
     hobbies = []
     rest_or_play = ""
-    wakeUp = ""
-    sleep = ""
+    wakeUp = datetime.strptime('10:00AM', '%I:%M%p').time()
+    sleep = datetime.strptime('11:00PM', '%I:%M%p').time()
 
     def __init__(self, name, age, food, drink, smoke, date, hobbies, rest_or_play, wakeUp, sleep):
        self.name = name
@@ -23,9 +24,8 @@ class UserProfile:
        self.date = date
        self.hobbies = hobbies
        self.rest_or_play = rest_or_play
-       self.wakeUp = wakeUp
-       self.sleep = sleep
-
+       self.wakeUp = datetime.strptime(wakeUp, '%I:%M%p').time()
+       self.sleep = datetime.strptime(sleep, '%I:%M%p').time()
     '''
     checks if user is over 21
     returns: true if user is over 21 and false otherwise
@@ -33,9 +33,7 @@ class UserProfile:
     def is_over_21(self):
         return int(self.age) >= 21
     def hours_awake(self):
-        #TODO
-        return 10
-    
+        return ((datetime.combine(date.min, self.sleep) - datetime.combine(date.min, self.wakeUp)).seconds)/3600
 
 '''
 profile: JSON file for each USER made by the app
@@ -65,10 +63,14 @@ class UserTrip:
        self.friends_family_romantic = who
        self.hotel_address = hotel_address
        self.lat_long = (geolocator.geocode(hotel_address).latitude, geolocator.geocode(hotel_address).longitude)
-       self.time_of_visit = time_of_visit
+       date_time_array = []
+       for time in time_of_visit:
+               d = datetime.strptime(time, '%m/%d/%Y').date()
+               date_time_array.append(d)                
+       self.time_of_visit = date_time_array
     def num_days(self):
-        #TODO
-        return 2
+        diff = (self.time_of_visit[self.time_of_visit.__len__() - 1] - self.time_of_visit[0])
+        return diff.days
 
 '''
 usertrip: JSON file for each USER made by the app
@@ -79,5 +81,6 @@ def parseUserTrip(usertrip):
     with open(usertrip) as json_file:
         trip_dict = json.loads(json_file.read())
         user = UserTrip(trip_dict['travelers'], trip_dict['who'], trip_dict['hotel_address'], trip_dict['time_of_visit'])
+        print(user.time_of_visit)
         return user
 
